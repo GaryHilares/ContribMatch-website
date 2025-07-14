eb6666d7b0780c5535b254f01560a1d79ba63997
floorence <fshen0426@gmail.com>
2025-07-14 11:25:58 -0700

    sign in and register pages

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,9 +1,49 @@
-import { Header } from "../components/layout/Header"
+import { Link } from "react-router-dom";
+import { Header } from "../components/layout/Header";
+import octocat from "../assets/octocat.png";
 
 const Register = () => {
     return (
-        <div>
+        <div className="flex flex-col">
             <Header />
+            <section className="flex flex-row">
+                <div className="flex flex-col w-1/2">
+                    <h1 className="font-bold text-7xl p-8">
+                        Sign up
+                    </h1>
+                    <h2 className="text-3xl mx-5">
+                        If you have an account you can
+                    </h2>
+                    <Link 
+                        to="/signin"
+                        className="text-3xl text-bold text-[#3B90CD] cursor-pointer mx-5"
+                    >
+                        Sign In here!
+                    </Link>
+                    <label className="text-3xl mt-4 mx-5">
+                        Full Name
+                    </label>
+                    <input type="text" className="rounded-xl bg-[#cccccc] m-4 p-2 h-10"/>
+                    <label className="text-3xl mx-5">
+                        Password
+                    </label>
+                    <input type="password" className="rounded-xl bg-[#cccccc] m-4 p-2 h-10"/>
+                    <label className="text-3xl mx-5">
+                        Confirm Password
+                    </label>
+                    <input type="password" className="rounded-xl bg-[#cccccc] m-4 p-2 h-10"/>
+                    <button className="self-end rounded border border-black text-base cursor-pointer bg-[#cccccc] p-2 m-4 w-1/5">
+                        Sign In
+                    </button>
+                </div>
+                <div className="relative flex w-1/2 h-screen">
+                    <div className="w-1/2"></div>
+                    <div className="w-1/2 bg-[#C3E5FE]"></div>
+                    <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10">
+                        <img src = {octocat} alt = "Octocat"></img>
+                    </div>
+                </div>
+            </section>
         </div>
     );
 };
--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,21 +1,22 @@
 import { Link } from "react-router-dom";
 import { Header } from "../components/layout/Header";
+import octocat from "../assets/octocat.png";
 
 const SignIn = () => {
     return (
         <div className="flex flex-col">
             <Header />
             <section className="flex flex-row">
-                <div className="flex flex-col">
+                <div className="flex flex-col w-1/2">
                     <h1 className="font-bold text-7xl p-8">
                         Sign in to ContribMatch
                     </h1>
-                    <h2 className="text-5xl mx-5">
+                    <h2 className="text-3xl mx-5">
                         If you don't have an account you can
                     </h2>
                     <Link 
                         to="/register"
-                        className="text-5xl text-bold text-[#70b3fe] cursor-pointer mx-5"
+                        className="text-3xl text-bold text-[#3B90CD] cursor-pointer mx-5"
                     >
                         Register Here!
                     </Link>
@@ -23,10 +24,25 @@ const SignIn = () => {
                         Email
                     </label>
                     <input type="text" className="rounded-xl bg-[#cccccc] m-4 p-2 h-10"/>
-                    <label className="text-3xl mx-5">
-                        Password
-                    </label>
+                    <div className="flex justify-between">
+                        <label className="text-3xl mx-5">
+                            Password
+                        </label>
+                        <button className="text-sm cursor-pointer pt-3 mx-4">
+                            Forgot Password?
+                        </button>
+                    </div>
                     <input type="password" className="rounded-xl bg-[#cccccc] m-4 p-2 h-10"/>
+                    <button className="self-end rounded border border-black text-base cursor-pointer bg-[#cccccc] p-2 m-4 w-1/5">
+                        Sign In
+                    </button>
+                </div>
+                <div className="relative flex w-1/2 h-screen">
+                    <div className="w-1/2"></div>
+                    <div className="w-1/2 bg-[#E5BFBF]"></div>
+                    <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10">
+                        <img src = {octocat} alt = "Octocat"></img>
+                    </div>
                 </div>
             </section>
         </div>
